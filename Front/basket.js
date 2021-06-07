014810a92475006c8ee8bdb681feabb849a5d08f
Hilali <Abdelali>
2021-06-07 11:41:14 +0200

    calcul total nbr articles et prix total panier

--- Front/basket.js
+++ Front/basket.js
@@ -2,26 +2,54 @@
 
 //fonction : recuperation du contenu du local storage dans le pannier
 
-const getProductInLocalStorage = key => {
+const getProductInLocalStorage = () => {
     let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
-    for(let i = 0; i <productInLocalStorage.length; i++){
-        const reducer = (accumulator, currentValue) => accumulator + productInLocalStorage[i].quantity;
-        productInLocalStorage.reduce(reducer)
-
-    }
-    localStorage.getItem("product", JSON.stringify(productInLocalStorage));
+    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
     return productInLocalStorage
 }
 
 
+// fonction : calcul du total des articles ajoutés au panier
 
-// fonction : affichage conetnu panier
+const CalcTotalTeddies = ()=>{
+    const totalQuantity =[];
+    for(let i=0; i<getProductInLocalStorage().length; i++) {
+        totalQuantity.push(getProductInLocalStorage()[i].quantity)
+    }
+   return  totalQuantity.reduce((accumulator,currentValue)=>{
+        return accumulator + currentValue;
+    },0)
+
+}
+
+
+// fonction : calcul du prix total des articles ajoutés au panier
+
+const totalPrice = ()=> {
+    const CalcTotalPrice = () => {
+        const prices = [];
+        for (let i = 0; i < getProductInLocalStorage().length; i++) {
+            const price = (getProductInLocalStorage()[i].price * getProductInLocalStorage()[i].quantity) / 100;
+            //parseInt(price, 10);
+            prices.push(price)
+        }
+        return prices.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue;
+        }, 0)
+
+    }
+   return  (new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR'}).format(CalcTotalPrice()));
+}
+
+
+
+// fonction : affichage du contenu panier
 
 const displayBasket = () =>{
 
         for(let i=0; i<getProductInLocalStorage().length; i++){
 
-            const priceEuro = (new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR'}).format(getProductInLocalStorage()[i].price/100));
+            const priceEuro = (new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR'}).format((getProductInLocalStorage()[i].price* getProductInLocalStorage()[i].quantity)/100));
             const templateBasket = document.getElementById("teddy-in-basket");
             const cloneSection = document.importNode(templateBasket.content, true);
 
@@ -29,10 +57,12 @@ const displayBasket = () =>{
             cloneSection.getElementById("js-price—basket").textContent ="Prix: " + priceEuro;
             cloneSection.getElementById("js-color-basket").textContent = "Couleur: " + getProductInLocalStorage()[i].colors;
             cloneSection.getElementById("js-quantity—basket").textContent = "Quantité: " + getProductInLocalStorage()[i].quantity;
-            cloneSection.getElementById("js-img—basket").setAttribute("src", getProductInLocalStorage()[i].imageUrl)
+            cloneSection.querySelector(".js-img—basket").setAttribute("src", getProductInLocalStorage()[i].imageUrl);
             document.getElementById("main-basket").appendChild(cloneSection);
         }
 
+    document.getElementById("js-total-articles-basket").textContent = CalcTotalTeddies();
+    document.getElementById("js-total-price-basket").textContent = totalPrice();
 }
 
 // recuperation et affichage du contenu panier apres chargement du dom
@@ -40,4 +70,6 @@ const displayBasket = () =>{
 document.addEventListener('DOMContentLoaded', () => {
     getProductInLocalStorage();
     displayBasket();
+    CalcTotalTeddies();
+    totalPrice();
 })
